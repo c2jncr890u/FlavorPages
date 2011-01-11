8af7774147ffcfc41e1cf683517ac7ec28358b98
Drew <c2jncr890u@gmail.com>
2011-01-11 03:16:48 -0500

    Updated Bash restart.

--- restart.sh
+++ restart.sh
@@ -4,9 +4,9 @@ pkill python
 
 cp /var/FlavorPages/nginx.conf /etc/nginx/
 
-python /var/FlavorPages/app/__init__.py 8000
-python /var/FlavorPages/app/__init__.py 8001
-python /var/FlavorPages/app/__init__.py 8002
-python /var/FlavorPages/app/__init__.py 8003
+python /var/FlavorPages/app/__init__.py 8000&
+python /var/FlavorPages/app/__init__.py 8001&
+python /var/FlavorPages/app/__init__.py 8002&
+python /var/FlavorPages/app/__init__.py 8003&
 
 /etc/init.d/nginx start
